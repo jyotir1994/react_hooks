c53bd24c5c3913e48fb58ce082844b14730b1893
impressico <jyotir1994@gmail.com>
2020-05-03 15:29:02 +0530

    useState

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HooksCounter from './components/HooksCounter';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ClassCounter /> */}
+      <HooksCounter />
+    </div>
+  );
+}
+
+export default App;
