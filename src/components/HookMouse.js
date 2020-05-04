b128a0f26a25abfa31de165beda3d0e450c25895
impressico <jyotir1994@gmail.com>
2020-05-04 17:43:36 +0530

    useEffect with cleanup

--- src/components/HookMouse.js
+++ src/components/HookMouse.js
@@ -5,6 +5,7 @@ function HookMouse() {
     const [y, setY] = useState(0)
 
     const logMousePosition = e => {
+        console.log('Mouse event')
         setX(e.clientX)
         setY(e.clientY)
     }
@@ -12,6 +13,11 @@ function HookMouse() {
     useEffect(() => {
         console.log('useEffect')
         window.addEventListener('mousemove', logMousePosition)
+
+        return() => {
+            console.log('Component unmounting code')
+            window.removeEventListener('mousemove', logMousePosition)
+        }
     }, [])    // [] for not always call useEffect after render
     return (
         <div>
