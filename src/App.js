2f6d394498974f5b4c20ad5ee480464aae60b7b0
impressico <jyotir1994@gmail.com>
2020-05-08 15:40:46 +0530

    multiple useReducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import CounterOne from './components/CounterOne';
-import CounterTwo from './components/CounterTwo';
+import CounterThree from './components/CounterThree';
 
 function App() {
   return (
     <div className="App">
-      {/* <CounterOne /> */}
-      <CounterTwo />
+      <CounterThree />
     </div>
   );
 }
